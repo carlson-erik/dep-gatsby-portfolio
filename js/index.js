ff9e00ef956eda7797ac15592d06d40de6237629
Erik Carlson <carlsonerikw@gmail.com>
2019-06-08 19:59:56 -0400

    Add start of new design. Add start of structural/ui work for header/about me sections.

---
+++ js/index.js
@@ -0,0 +1,7 @@
+// This function attaches all needed event listeners
+function attachListeners(){
+    console.log("this is where I will be attaching listeners")
+}
+
+// On load of page, attach all event listeners
+document.addEventListener('DOMContentLoaded', attachListeners, false);
