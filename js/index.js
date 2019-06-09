fffacca2aa42b3ef1abaca7e30bf2f1c4889755c
Erik Carlson <carlsonerikw@gmail.com>
2019-06-09 10:46:51 -0400

    Finish switching 'role' in header. Clean-up js. Move fontawesome to vendor folder.

--- js/index.js
+++ js/index.js
@@ -1,31 +1,29 @@
-// This function attaches all needed event listeners
+// attaches all needed event listeners
 function attachListeners(){
-    console.log("this is where I will be attaching listeners")
-    var intervalID = window.setInterval(incrementNextIndex, 1500);
+    var intervalID = window.setInterval(switchRoleElement, 1500);
 }
 
-function incrementNextIndex() {
+// handles incrementing all of the z-indexes for role elements; this does the switch of the "role" in header
+function switchRoleElement() {
     // if we haven't gotten the role elements, do that now
     if(!this.roleElements){
         this.roleElements = document.getElementsByClassName('role');
     }
-
     // if we haven't started increasing the z-index of the role elements, start the the first
     if(!this.roleIndex){
         this.roleIndex = 0;
     }
-
     // get current index in the array
     var currentIndex = this.roleIndex % this.roleElements.length;
     // get current element
     var currentElement = this.roleElements.item(currentIndex);
-    
+    // increment roleIndex
+    this.roleIndex += 1;
+    // use new roleIndex as z-index (z will always be > 1 )
     currentElement.style.zIndex = this.roleIndex;
-
-    console.log("incrementNextIndex ", currentElement, "something new", this.roleIndex++);
-
 }
 
+// run immediately
 (function(){
     document.addEventListener('DOMContentLoaded', attachListeners, false);
 }());
